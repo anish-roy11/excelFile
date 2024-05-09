55df0249da617d9d1d9d1357d4f1e74593bb0c76
anish-roy11 <121768208+anish-roy11@users.noreply.github.com>
2024-05-08 20:24:50 -0500

    modified

--- test/src/main/java/com/example/test/TestApplication.java
+++ test/src/main/java/com/example/test/TestApplication.java
@@ -19,57 +19,17 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-//@EnableSwagger2
-//@ComponentScan({"com.example.test", "com.example"})
-//@ComponentScan({"com.example.test"})
-//@EnableAutoConfiguration
-//public class FileTestApplication implements CommandLineRunner {
-//public class FileTestApplication implements ApplicationRunner {
-public class TestApplication implements ApplicationRunner, CommandLineRunner{
+public class TestApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(TestApplication.class, args);
-		System.out.println("logEntry:FileTestApplication.main method");
-		//SpringApplication.run(TestApplication.class, args);
-		ApplicationContext ctx = SpringApplication.run(TestApplication.class, args);
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-
-		Arrays.sort(beanNames);
-
-		for (String beanName : beanNames) {
-			System.out.println("sortedBeanNames="+beanName);
-		}
-
-		System.out.println("logExit:FileTestApplication.main method");
+		SpringApplication.run(TestApplication.class, args);
+		
 	}
 
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		System.out.println("logentry FileTestApplication.getRestTemplate method");
-		return new RestTemplate();
-	}
-
-
-
-	@Override
-	public void run(ApplicationArguments arg0) throws Exception {
-		System.out.println("logEntry: FileTestApplication.run method which is overridden method of ApplicationRunner");
-
-		//List<EmployeeEntity> employeeEntityList = employeeRepository.findAll();
-		//employeeEntityList.forEach(System.out :: println);
-	}
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("logEntry: FileTestApplication.run method which is overridden method of CommandLineRunner");
-
-	}
+	
 
 }
